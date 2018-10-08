73842f268aef693680c391ae3a5a3ee4a44b4601
JonathanLebourg <jonjonlebourg@gmail.com>
2018-10-08 15:48:10 +0200

    first commit

---
+++ exercice5.js
@@ -0,0 +1,12 @@
+
+// on demande les nombres
+
+var firstNumber = prompt('Premier nombre ?');
+var secondNumber = prompt('Deuxieme nombre ?');
+
+// on calcule le produit et on l'affiche
+
+var result = parseInt(firstNumber) * secondNumber;
+if (isNaN(firstNumber)||isNaN(secondNumber)) {
+		alert('problème de saisie')}
+else(alert("Le produit de ces deux nombres est " + result));
---
+++ exercice5Id.js
@@ -0,0 +1,11 @@
+function calcul (){
+
+var firstNumber = document.getElementById('firstNumber').value;
+var secondNumber = document.getElementById('secondNumber').value;
+var result = (parseInt(firstNumber) * secondNumber).value;
+
+// if (isNaN(firstNumber)||isNaN(secondNumber)) {
+// 		alert('problème de saisie')}
+// else(
+alert("Le produit de ces deux nombres est " + result);
+}
